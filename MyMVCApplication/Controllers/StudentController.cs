bd5da0712608aeb3b7703a2a9135a9c774927698
javier_PC <javier.nunezleyes@gmail.com>
2019-02-23 16:39:17 -0300

    Commit inicial capacitacion MVC 5

---
+++ MyMVCApplication/Controllers/StudentController.cs
@@ -0,0 +1,46 @@
+﻿using MyMVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyMVCApplication.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IList<Student> Student = new List<Student>() {
+           new Student() { StudentId = 1, StudentName = "John", Age = 18 } ,
+           new Student() { StudentId = 2, StudentName = "Steve",  Age = 21 } ,
+           new Student() { StudentId = 3, StudentName = "Bill",  Age = 25 } ,
+           new Student() { StudentId = 4, StudentName = "Ram" , Age = 20 } ,
+           new Student() { StudentId = 5, StudentName = "Ron" , Age = 31 } ,
+           new Student() { StudentId = 4, StudentName = "Chris" , Age = 17 } ,
+           new Student() { StudentId = 4, StudentName = "Rob" , Age = 19 }
+        };
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View(Student);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            Student studen = Student.SingleOrDefault(x => x.StudentId == id);
+
+            return View(studen);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student)
+        {
+            Student.RemoveAt(student.StudentId);
+
+            Student.Add(student);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyMVCApplication/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyMVCApplication.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        [Display(Name = "Name")]
+        public string StudentName { get; set; }
+
+        public int Age { get; set; }
+    }
+}
